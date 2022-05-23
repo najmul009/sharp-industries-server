0f4efc97a14f1057aee72161e7f9069bc27f5c68
najmul009 <najmul.tk16@gmail.com>
2022-05-23 12:00:25 +0600

    mongodb added successfuly

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ app.use(express.json())
 require('dotenv').config();
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nirim.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run() {
+    try {
+        await client.connect()
+        const toolsCollection = client.db("sharp-db").collection("tools")
+
+
+        app.get('/tools', async (req, res) => {
+            const query = {}
+            const cursor = toolsCollection.find(query)
+            const items = await cursor.toArray()
+            res.send(items)
+        })
+
+    }
+    finally {
+    }
+}
+run().catch(console.dir)
+
+
 app.get('/', (req, res) => {
     res.send('server started')
 })
